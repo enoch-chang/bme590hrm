197d94f783a37d048bddb3e085651271ebe520f1
Enoch Chang <enoch@Enochs-MacBook-Pro-3.local>
2018-02-28 16:53:39 -0500

    Updated num_beats and beats functions

--- process_ecg_data.py
+++ process_ecg_data.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy
+from peakdetect import peakdet
 
 class HeartRateMonitor:
 
-    def __init__(self, time, voltage):
+    def __init__(self, time, voltage, voltage_ref):
         self.time = time
         self.voltage = voltage
+        self.voltage_ref = voltage_ref
+        self.max_corr = []
 
     def mean_hr_bpm(self):
         """
@@ -13,8 +16,8 @@ class HeartRateMonitor:
         Calculates the mean heart rate in beats per minute.
         :return mean_hr_bpm: mean heart rate
         """
-
-        mean_hr_bpm = self.num_beats()/self.duration()
+        duration_min = self.duration()/60
+        mean_hr_bpm = self.num_beats()/duration_min
 
         return mean_hr_bpm
 
@@ -26,8 +29,8 @@ class HeartRateMonitor:
         """
 
         voltage_extremes = []
-        voltage_extremes[0] = min(self.voltage)
-        voltage_extremes[1] = max(self.voltage)
+        voltage_extremes.append(min(self.voltage))
+        voltage_extremes.append(max(self.voltage))
 
         return voltage_extremes
 
@@ -48,12 +51,26 @@ class HeartRateMonitor:
         Determines the number of beats in the data set
         :return num_beats: number of beats in the data set
         """
-        corr_voltage = numpy.correlate(self.voltage, self.voltage, mode = 'valid')
+        norm_voltage = self.voltage - numpy.mean(self.voltage)
+        norm_voltage_ref = list(self.voltage_ref - numpy.mean(self.voltage_ref))
+        ref_index = norm_voltage_ref.index(max(norm_voltage_ref))
+        corr_voltage = numpy.convolve(norm_voltage, norm_voltage_ref[ref_index-30:ref_index+30], mode='same')
+        # corr_voltage = numpy.convolve(norm_voltage, norm_voltage_ref[144:504], mode = 'same')
+
+        min_corr = []
+        voltage_delta = numpy.sqrt(numpy.mean(corr_voltage**2))*2.5
+        print(voltage_delta)
+        [self.max_corr, min_corr] = peakdet(corr_voltage, voltage_delta, self.time)
+        max_corr_voltage = []
+        for i in self.max_corr:
+            max_corr_voltage.append(i[0])
+        num_beats = len(max_corr_voltage)
+
         print(corr_voltage)
         plt.plot(corr_voltage)
         plt.show()
 
-        return
+        return num_beats
 
     def beats(self):
         """
@@ -61,5 +78,9 @@ class HeartRateMonitor:
         Finds the times corresponding to a heart beat
         :return beats: a list containing the times for each beat
         """
+        beats = []
+
+        for i in self.max_corr:
+            beats.append(i[1])
 
         return beats

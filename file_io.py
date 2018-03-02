6a7d135f0eae17a65d7adff853a18a56637436e1
Enoch Chang <enoch@Enochs-MacBook-Pro-3.local>
2018-03-02 00:20:08 -0500

    Made file_io.py more modular

--- file_io.py
+++ file_io.py
@@ -2,39 +2,34 @@ import glob
 import csv
 import json
 
-def collect_csv_filenames():
+def collect_csv_filenames(dir):
     """Collect all the *.csv files located in the test_data folder
 
     :return: list containing list of all filenames
     """
-    filenames_list = glob.glob('./test_data/*.csv')
+    filenames_list = glob.glob(dir)
     if len(filenames_list) == 0:
         print("No files found in specified directory.")
     return filenames_list
 
-def read_csv(filename, time_unit="sec"):
+def read_csv(filename):
     """Read csv file and separate time and voltage into respective lists
 
     :param filename:
-    :param time_unit: users can input unit for timescale, if none specified, unit is assumed to be seconds
     :return:
     """
     csvfile = open(filename, "r")
     time = [ ]
     voltage = [ ]
     temp = csv.reader(csvfile, delimiter=",")
-    if time_unit != "sec" and time_unit != "min":
-        print("Warning: Input was neither 'sec' or 'min'. Data saved as seconds.")
     for row in temp:
-        if time_unit == "min":
-            time.append(floar(row[0])*60)
-        else:
-            time.append(float(row[0]))
-        voltage.append(float(row[1]))
+        time.append(row[0])
+        voltage.append(row[1])
+
     return time, voltage
 
 def write_json(filename, info, type = 'json'):
-    """
+    """Write
 
     :param filename:
     :param type:
